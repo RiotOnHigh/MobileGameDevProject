532e54dc79eb814d9bac6320e1b47c5d21423b89
RiotOnHigh <Thr33D0g>
2016-04-22 13:28:03 +0100

    refatoring of menu and game states have started

--- Js/main.js
+++ Js/main.js
@@ -4,40 +4,14 @@
     //Initiate the phaser framework
 var game = new Phaser.Game(640, 360, Phaser.CANVAS);
 
-var GameState =
-{
-    //Loads Assets for game before it starts
-    preload: function()
-    {
+    //Initiate the game state
+var game;
 
-        this.load.image('title', 'Assets/Images/PlaceholderTitle.png');
+//First parameter is how our state will be called
+//Second is an object containing the needed methods for state functionality
+game.state.add('menu', menu);
 
+// Adding the Game state.
+game.state.add('game', game);
 
-    },
-
-    //Executes everything after loaded
-    create: function()
-    {
-
-        this.titile = this.game.add.sprite(0, 0, 'title');
-        // For Screen Scaling
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;this.scale.minWidth = 480;this.scale.minHeight = 260;this.scale.maxWidth = 1024;this.scale.maxHeight = 768;this.scale.forceLandscape = true;this.scale.pageAlignHorizontally = true;
-
-    },
-
-    //This is executed multiple times per second
-    update: function()
-    {
-
-
-        
-    },
-};
-
-
-
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+game.state.start('menu');
