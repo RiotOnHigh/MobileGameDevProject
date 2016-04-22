b0faf60dae4f921758149dc6d0d0957596a07643
RiotOnHigh <kmcgivern95@googlemail.com>
2016-04-22 09:59:04 +0100

    Basic Window Added

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,29 @@
 /**
  * Created by B00252361 on 22/04/2016.
  */
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+var GameState =
+{
+
+    preload: function()
+    {
+
+
+    },
+
+    create: function()
+    {
+
+
+    },
+
+    update: function()
+    {
+
+        
+    },
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
